55d5a7cdbf883ec0a6263473b95b09bfab852fa6
Saveli Matjuhhin <saveli.matjuhhin@tuni.fi>
2025-01-17 14:30:17 +0200

    Exercise 11.16: Test #skip

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('0.1.1') // change this string to ensure a new version deployed
+  res.send('0.0.2') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
